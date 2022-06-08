7133f93096fdb8d016163f485a4ba1a0bc135098
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2022-06-08 19:06:24 +0200

    Delete texttospeech.py

--- Toolkit/texttospeech.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import os
-from playsound import playsound
-def tts(text:str,axc:str,lng:str,u_dir:str):
-    gTTS(text, "com" + axc, lng).save(f"{u_dir}/tmpSound.mp3")
-    playsound(f"{u_dir}tmpSound.mp3")
-    os.remove(f"{u_dir}tmpSound.mp3")
